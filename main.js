2b425e3d716c332c8b0e684c2dec9768042ead53
Ben Singer <benjamin.singer@helpsystems.com>
2015-02-25 15:40:47 -0600

    initial

---
+++ main.js
@@ -0,0 +1,80 @@
+var play = $('.play');
+var pause = $('.pause');
+var songs = $('#songs');
+var search = $('#search');
+
+SC.initialize({
+    client_id: '36795704793d977a265b546b855f7ecb',
+    redirect_uri: 'http://localhost:8080/'
+});
+
+SC.get('/tracks', { q: '' }, function(tracks) {
+    
+});
+
+
+
+var populateSongs = function(text){
+    songs.empty();
+    SC.get('/tracks', { q: text }, function(tracks) {
+        if(tracks.length > 0){
+            var table = $('<table></table>');
+            table.addClass('table table-striped');
+            
+            var headerTr = $('<tr></tr>');
+            headerTr.append($('<td><b>Title</b></td>').addClass("title"));    
+            headerTr.append($('<td><b>Artist</b></td>').addClass("artist")); 
+            headerTr.append($('<td></td>').addClass("artist")); 
+            headerTr.append($('<td></td>').addClass("artist")); 
+            table.append(headerTr);
+            
+            tracks.forEach(function(track){
+                var tr = $('<tr></tr>');
+                tr.append($('<td>' + track.title + '</td>').addClass("title"));    
+                tr.append($('<td>' + track.user.username + '</td>').addClass("artist"));
+                var play = $('<button class="btn play" id="' + track.id + '">Play</button>');
+                var pause = $('<button class="btn pause" id="' + track.id + '">Pause</button>');
+                
+                var soundPlaying;
+                play.click(function(e){
+                    if(!soundPlaying){
+                        SC.stream("/tracks/" + e.currentTarget.id, function(sound){
+                            sound.play();
+                            soundPlaying = sound;
+                        });
+                    }else{
+                        if(soundPlaying.paused){
+                            soundPlaying.play();
+                        }
+                    }  
+                });
+                pause.click(function(e){
+                    if(soundPlaying){
+                        soundPlaying.pause();
+                    }
+                }); 
+
+
+                var playTd = $('<td></td>');
+                var pauseTd = $('<td></td>');
+                playTd.append(play);
+                pauseTd.append(pause);
+                
+                tr.append(playTd);
+                tr.append(pauseTd);
+                table.append(tr);
+            });           
+            songs.append(table);
+            
+            
+
+
+        }
+    });
+};
+
+search.keyup(function(e){
+    console.log(e.currentTarget.value);
+    populateSongs(e.currentTarget.value);
+});
+
