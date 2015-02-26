a0fb6e6def8d374d56f62f5817aa24fb943958c9
Ben Singer <benjaminjsinger@gmail.com>
2015-02-25 22:51:51 -0600

    started making player, remembering file tho

---
+++ player.js
@@ -0,0 +1,38 @@
+var Player = function(){
+  SC.initialize({
+      client_id: '36795704793d977a265b546b855f7ecb',
+      redirect_uri: 'http://localhost:8080/'
+  });
+                  
+  var currentSong;
+  var future = [];
+  var history = [];
+  var queue = [];
+  var volume;
+  
+  return {
+    play: function(){
+
+    },
+    pause: function(){
+
+    },
+    stop: function(){
+
+    },
+    back: function(){
+
+    },
+    foward: function(){
+
+    },
+    restart: function(){
+
+    },
+    volumeUp: function(){
+
+    },
+    volumeDown: function(){
+
+    }
+};
