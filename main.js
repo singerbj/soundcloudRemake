82ba72d35c3cb3e120db44a3240a7658ac558cda
Ben Singer <benjamin.singer@helpsystems.com>
2015-02-27 15:43:46 -0600

    more enhancements on the listing

--- main.js
+++ main.js
@@ -1,61 +1,87 @@
-var back = $('#back');
-var play = $('#play');
-var pause = $('#pause');
-var forward = $('#forward');
+$(document).ready(function(){
+    var back = $('#back');
+    var play = $('#play');
+    var pause = $('#pause');
+    var forward = $('#forward');
 
-var songs = $('#songs');
-var search = $('#search');
+    var songs = $('#songs');
+    var search = $('#search');
 
+    var lastClicked;
 
-var player = new Player();
-window.p = player;
-console.log(player);
+    var player = new Player();
+    window.p = player;
+    console.log(player);
 
-back.click(function(){
-    player.backSong();
-});
-play.click(function(){
-    player.playSong();
-});
-pause.click(function(){
-    player.pauseSong();
-});
-forward.click(function(){
-    player.forwardSong();
-});
-
-
-
-
-
-var populateSongs = function(text){
-    songs.empty();
-    SC.get('/tracks', { q: text }, function(tracks) {
-        if(tracks.length > 0){
-            player.loadSongs(tracks.map(function(track){
-                return track.id;
-            }));
-
-            var table = $('<table></table>');
-            table.addClass('table table-striped');
-            
-            var headerTr = $('<tr></tr>');
-            headerTr.append($('<td><b>Title</b></td>').addClass("title"));    
-            headerTr.append($('<td><b>Artist</b></td>').addClass("artist")); 
-            table.append(headerTr);
-            
-            tracks.forEach(function(track){
-                var tr = $('<tr></tr>');
-                tr.append($('<td>' + track.title + '</td>').addClass("title"));    
-                tr.append($('<td>' + track.user.username + '</td>').addClass("artist"));
-                table.append(tr);
-            });           
-            songs.append(table);
-        }
+    back.click(function(){
+        player.backSong();
     });
-};
+    play.click(function(){
+        player.playSong();
+    });
+    pause.click(function(){
+        player.pauseSong();
+    });
+    forward.click(function(){
+        player.forwardSong();
+    });
 
-search.keyup(function(e){    
-    populateSongs(e.currentTarget.value);
+
+
+
+    var populateSongs = function(text){
+        songs.empty();
+        SC.get('/tracks', { q: text }, function(tracks) {
+            if(tracks.length > 0){
+                player.loadSongs(tracks.filter(function(track){
+                    return track.track_type === 'original';
+                }).map(function(track){
+                    console.log(track);
+                    return track.id;
+                }));
+
+                var table = $('<table></table>');
+                table.addClass('table table-striped');
+                
+                var headerTr = $('<tr></tr>');
+                headerTr.append($('<td><b>Title</b></td>').addClass("title"));    
+                headerTr.append($('<td><b>Artist</b></td>').addClass("artist")); 
+                table.append(headerTr);
+                
+                var songIndex = 0;
+                tracks.forEach(function(track){
+                    var tr = $('<tr id=' + track.id + '></tr>');
+                    tr.append($('<td>' + track.title + '</td>').addClass("title"));    
+                    tr.append($('<td>' + track.user.username + '</td>').addClass("artist"));
+                    tr.songIndex = songIndex;
+
+                    tr.click(function(e){
+                        player.loadSongs(tracks.slice(tr.songIndex).map(function(track){
+                            return track.id;
+                        }));
+                        player.forwardSong();
+
+                        if(lastClicked){ lastClicked.className = ""; }
+                        lastClicked = e.target.parentElement;
+                        lastClicked.className = "clicked";
+
+                    });
+
+                    table.append(tr);
+                    songIndex += 1;
+                });           
+                songs.append(table);
+            }
+        });
+    };
+
+    $(document).ready(function(){
+        populateSongs("g-eazy");
+        search[0].value = "g-eazy";
+    })
+
+    search.keyup(function(e){    
+        populateSongs(e.currentTarget.value);
+    });
+
 });
-
--- player.js
+++ player.js
@@ -15,34 +15,42 @@ var Player = function(){
   });
 
   
-  self.playSong = function(){      
-    if(self.currentSong){      
-      if(!self.volume){ self.volume = 50; }
-      self.currentSong.setVolume(self.volume);        
-      self.currentSong.start();
-      self.currentSong._onfinish(function(){
-        self.forwardSong();
-      });
-    }else if(!self.currentSong){
-      self.forwardSong();
-    }
+  self.playSong = function(songId){     
+      if(songId){      
+        if(!self.volume){ self.volume = 50; }
+        SC.stream("/tracks/" + songId, function(sound){          
+          if(self.currentSong){ self.currentSong.sound.unload(); }
+          self.currentSong = {sound: sound, id: songId};
+          window.currentSong = {sound: sound, id: songId};
+          self.currentSong.sound.setVolume(self.volume);        
+          self.currentSong.sound.play();
+          self.currentSong.sound._onfinish(function(){
+            self.forwardSong();
+          });
+        });
+      }else{
+        if(!self.currentSong){
+          self.forwardSong();
+        }else{
+          console.log(self.currentSong.sound.paused);
+          if(self.currentSong.sound.paused === true){
+            self.currentSong.sound.play();
+          }
+        }
+      }    
   }
   self.pauseSong = function(){
     if(self.currentSong){
-      self.currentSong.pause();
+      self.currentSong.sound.pause();
     }
   }    
   self.backSong = function(){
     if(self.history.length > 0){   
-      if(self.currentSong && self.currentSong.position < 1500 && self.history.length > 0){
-        self.currentSong.unload();
-        self.future.unshift(parseInt(self.currentSong.sID.replace('T/tracks/', '').split('-')[0], 10));
-        SC.stream("/tracks/" + self.history.splice(self.history.length - 1, 1)[0], function(sound){
-          self.currentSong = sound;
-          self.playSong();
-        });
+      if(self.currentSong && self.currentSong.sound.position < 1500 && self.history.length > 0){
+        self.future.unshift(self.currentSong.id);        
+        self.playSong(self.history.splice(self.history.length - 1, 1)[0]);
       }else{      
-        self.currentSong.setPosition(0);
+        self.currentSong.sound.setPosition(0);
         self.playSong();
       }
     }
@@ -50,15 +58,10 @@ var Player = function(){
   self.forwardSong = function(){
     if(self.future.length > 0){
       if(self.currentSong){         
-        self.currentSong.unload();
-        self.history.push(parseInt(self.currentSong.sID.replace('T/tracks/', '').split('-')[0], 10));
-      }    
-      SC.stream("/tracks/" + self.future.splice(0, 1)[0], function(sound){
-        self.currentSong = sound;
-        self.playSong();
-      });
-    }
-    
+        self.history.push(self.currentSong.id);
+      }          
+      self.playSong(self.future.splice(0, 1)[0]);
+    } 
   }
   self.loadSongs = function(arrayOfSongIds){
     self.future = arrayOfSongIds;
